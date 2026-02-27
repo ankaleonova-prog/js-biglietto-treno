d188c1ec0b3245d0a003079fbd1189edbc6f4b73
Ankaleonova <ankaleonova@gmail.com>
2026-02-27 12:39:28 +0100

    add colculated price

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //il biglieto treno//
 //recupero dati da input//
 const km = parseFloat(document.getElementById('km').value)
-const age =parseInt(document.getElementById('age').value)
+const age = parseInt(document.getElementById('age').value)
 
 //calcolare prezzo//
 let price = km * 0.21
@@ -11,3 +11,9 @@ if (age === 'minorenne') {
     } else if (age === 'over65') {
         price *= 0.6; // 40% sconto
     }
+
+    //mostra il prezzo
+
+document.getElementById('ticket-km').innerText = km;
+document.getElementById('ticket-price').innerText = price.toFixed(2);
+document.getElementById('ticket').classList.remove('hidden'); 
