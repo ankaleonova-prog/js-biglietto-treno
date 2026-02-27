35631dafb2c80c7131836d33b039652f50137abf
Ankaleonova <ankaleonova@gmail.com>
2026-02-27 12:33:05 +0100

    add price and sales

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 //il biglieto treno//
+//recupero dati da input//
 const km = parseFloat(document.getElementById('km').value)
-const age = parseInt(document.getElementById('age').value)
+const age =parseInt(document.getElementById('age').value)
+
+//calcolare prezzo//
+let price = km * 0.21
+//sconto//
+if (age === 'minorenne') {
+        price *= 0.8; // 20% sconto
+    } else if (age === 'over65') {
+        price *= 0.6; // 40% sconto
+    }
