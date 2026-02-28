cba9037bb0715396d87ae0aac90e597c8f363c2d
Ankaleonova <ankaleonova@gmail.com>
2026-02-28 10:41:46 +0100

    add sale

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ console.log (km)
 //colculated price
 const pricekm = 0.21 
 let price = km * pricekm
-
-
 //passenger age
 const age = parseInt(prompt("scrivere un numero"));
 console.log (age)
  //add sales
+ if (age < 18) {
+        price *= 0.8; // sale 20%
+    } else if (age > 65) {
+        price *= 0.6; // sale 40%
+    }
