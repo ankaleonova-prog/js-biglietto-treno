47772d6e73845e10ce6fb556001fd49d94851d36
Ankaleonova <ankaleonova@gmail.com>
2026-02-28 10:28:41 +0100

    add km

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const
+//collect info
+const km = parseInt (prompt ("Scrivi km da percorrere"))
+console.log (km)
+//colculated price
 
+
+//passenger age
 const age = parseInt(prompt("scrivere un numero"));
 console.log (age)
+ //add sales
